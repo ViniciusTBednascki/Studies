e38bd89035e5ac68c7750fb5d148222bfc4f73c9
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-02-02 21:38:28 -0300

    Dotnet - csharp

---
+++ BootcampGFT/dotnet_csharp/prática/Aluno.cs
@@ -0,0 +1,8 @@
+namespace prática
+{
+    public struct Aluno //o struct foi utilizado apenas como exemplo, oideal seria utilizar class
+    {
+        public string nome { get; set; }
+        public decimal nota { get; set; }
+    }
+}
---
+++ BootcampGFT/dotnet_csharp/prática/Conceito.cs
@@ -0,0 +1,11 @@
+namespace prática
+{
+    public enum Conceito
+    {
+        A,
+        B,
+        C,
+        D,
+        E
+    }
+}
