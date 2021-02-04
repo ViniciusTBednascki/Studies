bee1826a1dd8f118b989b5ffb5aa006554300346
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-02-04 15:47:47 -0300

    ASPdotnet class

--- BootcampGFT/dotnet_csharp/prática/Aluno.cs
+++
@@ -1,8 +0,0 @@
-namespace prática
-{
-    public struct Aluno //o struct foi utilizado apenas como exemplo, oideal seria utilizar class
-    {
-        public string nome { get; set; }
-        public decimal nota { get; set; }
-    }
-}
